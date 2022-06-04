dc0a429a30dd904eed66a5b4d27adf609c78890e
junteudjio <jun.teudjio@gmail.com>
2022-06-04 21:19:23 +0100

    fix image Domains bug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const imageDomains = [];
+
+if (process.env.CONTENTFUL_IMAGES_DOMAIN) imageDomains.push(process.env.CONTENTFUL_IMAGES_DOMAIN);
+if (process.env.STRAPI_IMAGES_DOMAIN) imageDomains.push(process.env.STRAPI_IMAGES_DOMAIN);
+if (process.env.DATOCMS_IMAGES_DOMAIN) imageDomains.push(process.env.DATOCMS_IMAGES_DOMAIN);
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [
-      process.env.CONTENTFUL_IMAGES_DOMAIN,
-      process.env.STRAPI_IMAGES_DOMAIN,
-      process.env.DATOCMS_IMAGES_DOMAIN,
-    ],
+    domains: imageDomains,
   },
 };
 
